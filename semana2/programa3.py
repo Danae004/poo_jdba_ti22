9316645d8614f437bc17c1fd7426de0f0566da46
Danae004 <122801105+Danae004@users.noreply.github.com>
2023-01-24 14:54:31 +0000

    Operaciones aritméticas    usando el "print" y el "format"

---
+++ semana2/programa3.py
@@ -0,0 +1,19 @@
+"""
+    Programa2
+    Nombre: Danae Barqueras A
+    Fecha: 24/01/2023
+    Descripcion: Operaciones aritmeticas 
+    usando el "print" y el "format"
+"""
+
+variable1 = 10 #declaracion de variables a usar
+variable2 = 5
+#Codigo para realizar la suma
+print("El resultado de la suma es {}+{}={}".format(variable1,variable2,variable1 + variable2))
+#realiza una resta
+print("El resultado de la resta es {}-{}={}".format(variable1,variable2,variable1 - variable2))
+
+#realiza una multiplicacion
+print("El resultado de la multiplicacion es {}*{}= {}".format(variable2,variable1,variable2 * variable1))
+#realiza una division
+print("El resultado de la division es {}/{}= {}".format(variable1,variable2,variable1 / variable2))
