f3d30ea8534d5f577c96c24d0757291780eb6c9c
Danae004 <122801105+Danae004@users.noreply.github.com>
2023-01-24 14:05:34 +0000

    Identificar algunos errores posibles del programa, usar comentarios de una sola línea y múltiple

---
+++ semana2/programa2.py
@@ -0,0 +1,13 @@
+"""
+    Programa2
+    Nombre: Danae Barqueras A
+    Fecha: 24/01/2023
+    Descripcion: formatear variables he 
+    identificar algunos errores que 
+    podria tener nuestro programa
+"""
+variable1= "Hola " #Primera variable a sumar
+variable2= 619 # Segunda variable a sumar
+
+# print(variable1 + variable2) #Suma ambas variables
+print("{}{}" .format(variable1,variable2)) #Suma ambas variables
