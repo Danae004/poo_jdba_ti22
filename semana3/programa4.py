bb7f84af48bb45d71738c6e3c7e609474a26c273
Danae004 <122801105+Danae004@users.noreply.github.com>
2023-01-30 14:55:11 +0000

    terminar el programa

---
+++ semana3/programa4.py
@@ -0,0 +1,22 @@
+"""
+    Programa4
+    Nombre: Danae Barqueras A
+    Fecha: 30/01/2023
+    Descripcion: casting
+"""
+numero1 = input("Numero 1: ")
+numero2 = 10
+suma = int(numero1) + numero2
+
+resta = int(numero1) - numero2
+
+division= int(numero1) / numero2
+
+multiplicacion = (numero1) * numero2
+
+potencia = int(
+print(suma)
+print(resta)
+print(division)
+print(multiplicacion)
+print(potencia)
