beb28f324638aefbd6f01eba30fdf7d8b1ff2ba8
Danae004 <122801105+Danae004@users.noreply.github.com>
2023-02-04 00:43:25 +0000

    Código resuelto y funcionando :)

--- semana3/programa7.py
+++ semana3/programa7.py
@@ -4,17 +4,18 @@
     Fecha: 01/02/2023
     Descripcion:Comparar 2 numeros enteros e imprimir el numero mayor
 """
-print("Este programa logra comparar dos numeros enteros para saber cual es el numero mayor ")#Indicaciones sobre nuestro programa
+#El proposito es comparar dos numeros enteros y obtener el mayor de ellos
+print("  Este programa logra comparar dos numeros enteros para saber cual es el numero mayor ") #Indicaciones sobre que puede hacer nuestro programa
 
-print("Bienvenido humano :)")
+print("Bienvenido humano :)")#Introduccion de bienvenida
 
-n1 =input("Coloca el primer numero entero: ")
-n2 =input("Coloca el segundo numero: ")
+n1 =input("Coloca el primer numero entero: ")#Colocar un numero
+n2 =input("Coloca el segundo numero: ")#Colocar un segundo numero
 
-if n1 > n2:
-    print(f"Humano el numero {n2} es mayor que {n1}")
+if n1 < n2:
+    print(f"Humano el numero {n2} es mayor que {n1}")#Primer desicion si el numero dos es mayor
 else:
-    print(f"Humano el numero {n1} es mayor que {n2}") 
+    print(f"Humano el numero {n1} es mayor que {n2}")#Segunda eleccion si el numero dos es menor. 
 
 
 
