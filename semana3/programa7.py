d0d7b2ff1645b7a07cf709956a2825a480bc497c
Danae004 <122801105+Danae004@users.noreply.github.com>
2023-02-08 03:50:49 +0000

    formas de obtener resultados

--- semana3/programa7.py
+++ semana3/programa7.py
@@ -5,17 +5,101 @@
     Descripcion:Comparar 2 numeros enteros e imprimir el numero mayor
 """
 #El proposito es comparar dos numeros enteros y obtener el mayor de ellos
-print("  Este programa logra comparar dos numeros enteros para saber cual es el numero mayor ") #Indicaciones sobre que puede hacer nuestro programa
+print(" Este programa logra comparar dos numeros enteros para saber cual es el numero mayor ") #Indicaciones sobre que puede hacer nuestro programa
 
 print("Bienvenido humano :)")#Introduccion de bienvenida
 
 n1 =input("Coloca el primer numero entero: ")#Colocar un numero
 n2 =input("Coloca el segundo numero: ")#Colocar un segundo numero
 
-if n1 < n2:
-    print(f"Humano el numero {n2} es mayor que {n1}")#Primer desicion si el numero dos es mayor
+if n1 > n2:
+    print(f"Humano el numero {n1} es mayor que {n2}")#Primer desicion si el numero dos es mayor
+
+elif n2 > n1 :
+    print(f"Humano el numero {n2} es mayor que {n1}")#Segunda eleccion si el numero dos es menor.
 else:
-    print(f"Humano el numero {n1} es mayor que {n2}")#Segunda eleccion si el numero dos es menor. 
+    print(None) #Ultima eleccion 
+
+#A continuacion se presentan formas diferentes de obtener resultados.
+#PRIMERA FORMA
+
+if n1 > n2 :
+    print(n1)
+    if n2 > n1 :
+        print(n2)
+if n1 == n2 :
+    print(None)
+
+#SEGUNDA FORMA
+    if n2 > n1 :
+        print(n2)
+        if n1 > n2 :
+            print(n1)
+    if n2 == n1 :
+        print(None)
+#TERCERA FORMA
+    if n1 > n2 :
+        print(n1)
+    elif n2 > n1 :
+        print(n2)
+    else:
+        print(None)
+#CUARTA FORMA
+    if n1 < n2 :
+        print(n2)
+    elif n2 < n1 :
+        print(n1)
+    else:
+        print(None)
+#QUINTA FORMA
+    if n2 < n1 :
+        print(n1)
+    if n1 < n2 :
+        print(n2)
+    if n1 == n2 :
+        print(None)
+#SEXTA FORMA
+    if n1 >= n2 :
+        if n1 > n2 :
+            print(n1)
+    else:
+        print(None)
+ 
+#SEPTIMA FORMA
+    if n1 <= n2 :
+        if n1 < n2:
+          print(n2)
+else:
+    print(None)
+
+#OCTABA FORMA
+    if n1 <= n2 :
+      if n1 == n2 :
+       print(n1)
+      else:
+          print(None)
+    else:
+            print(n2)
+#NOVENA FORMA
+if n1 == n2:
+    print(None)
+elif n1 > n2 :
+    print(n1)
+else:
+    print(n2)
+#DECIMA FORMA
+    if n2 == n1:
+        print(None)
+    elif n1 > n2:
+        print(n2)
+    else:
+        print(n1)
+
+#ONCEABA FORMA
+        
+            
+    
+
 
 
 
