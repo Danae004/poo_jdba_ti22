8239eca648b29413ff389287c11846035b024150
Danae004 <122801105+Danae004@users.noreply.github.com>
2023-02-10 02:02:47 +0000

    corregir

---
+++ semana4/programa.py
@@ -0,0 +1,9 @@
+
+
+def positivo_no_positivo():
+   
+    uno = input("Ingrese un numero: ")
+    if uno > 0 :
+       print ("Numero positivo")
+    if not (uno > 0) :
+        print ("Numero no positivo")
