95cb2b1bf6c1fc7a8948152218f204b4143853f4
Danae004 <122801105+Danae004@users.noreply.github.com>
2023-02-15 14:49:54 +0000

    funciona

--- semana3/programa5.py
+++ semana3/programa5.py
@@ -2,12 +2,14 @@
     Programa7
     Nombre: Danae Barqueras A
     Fecha: 01/02/2023
-    Descripcion:obtener el area de un triangulo
+    Descripcion:obtener el area de un 
+    triangulo
 """
 
-print ("ESTE PROGRAMA CALCULA EL AREA DE UN TRIANGULO")
-base=input("ESCRIBE LA BASE: ")
-altura=input("ESCRIBE LA ALTURA: ")
-area=int (base) * int (altura) / 2.0
+print ("Hola humano :)")
+print("Vamos a obtener la base de un triangulo") #Descripcion de lo que nuestro programa puede hacer
+base=input("Escribe la base: ") #Coloca el primer valor
+altura=input("Escribe la altura: ") #Coloca el segundo valor
+area=int (base) * int (altura) / 2.0 #Definimos las operaciones que usaremos para obtener la base
 print ("el resultado es: " + str (area))
-
+#Imprimimos el resultado
