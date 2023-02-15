425ccc26b1332b2e664d10fb2da655d7a6ee8157
Danae004 <122801105+Danae004@users.noreply.github.com>
2023-02-15 14:43:20 +0000

    funcionando

--- semana3/programa4.py
+++ semana3/programa4.py
@@ -4,16 +4,17 @@
     Fecha: 30/01/2023
     Descripcion: casting
 """
-numero1 = input("Numero 1: ")
+numero1 = input("Numero 1: ") #Colocar cualquier valor
+#definir las variables a usar
 numero2 = 10
-suma = int(numero1) + numero2
-
+suma = int(numero1) + numero2 # Colocar el valor a la variable
 resta = int(numero1) - numero2
+#Realizar modificaciones a cada variable dependiendo la funcion que quiera que aga
 
 division= int(numero1) / numero2
 
 multiplicacion = (numero1) * numero2
-
+#imprimimos los resultados dependiendo del numero colocado en la linea 7
 print(suma)
 print(resta)
 print(division)
