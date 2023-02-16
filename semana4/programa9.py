41b7e2c8505abb7f82e72a8372a54eb2de836ac3
Danae004 <122801105+Danae004@users.noreply.github.com>
2023-02-16 04:48:50 +0000

    funcionando

--- semana4/programa9.py
+++ semana4/programa9.py
@@ -4,7 +4,7 @@
     Fecha: 01/02/2023
     Descripcion:comparar y obtener un numero mayor
 """
-
+#Primero definimos cada variable y asignacion
 
 def mayor(numero1,numero2) :
     if numero1 > numero2 :
@@ -13,14 +13,14 @@ def mayor(numero1,numero2) :
          print(numero2)
     else:
          print(None)
-       
+       #Colocamos las formas en las que nuestro programa debe responder
 mayor(10,5)#3
 mayor(5,10)#3
 mayor(5,5)#None
+#Asignamos valores para ejecutar nuestro codigo
 
 
-
-
+#Colocamos las variables con las asignaciones
 def mayor(numero1:int,numero2:int)->int :
     mayor= None
     if numero1 > numero2 :
@@ -30,10 +30,11 @@ def mayor(numero1:int,numero2:int)->int :
         mayor= numero2
     else:
          mayor=None
-
+       #Damos las decisiones (o posibles) que puede tomar
     return mayor
 
 
 print(mayor(8,4))#3
 print(mayor(8,4))#3
 print(mayor(4,4))#None
+#Asignamos los valores he imprimimos
