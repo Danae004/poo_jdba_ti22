1e62d4906da1e299974e2634827a1b373a5832d5
Danae004 <122801105+Danae004@users.noreply.github.com>
2023-02-17 05:16:03 +0000

    codigo funcionando

---
+++ semana5/clase1.py
@@ -0,0 +1,9 @@
+class Persona: #Definir una clase
+	_nombre = None #Ponerle un nombre a la variable
+	def __init__(self):
+		print("Persona") #Imprimir el resultado pedido
+
+
+objeto = Persona() #Imprime el valor pedido
+dejah = Persona()
+print(objeto._nombre)
