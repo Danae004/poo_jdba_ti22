9f840cbb37ccd6e7fdbe55047de5c9df49daf6d5
Danae004 <122801105+Danae004@users.noreply.github.com>
2023-02-18 04:23:30 +0000

    funcional

---
+++ semana5/herencia.py
@@ -0,0 +1,36 @@
+"""
+Nombre: Josandy Danae B.A.
+FECHA: 14/02/23
+DESCRIPCION: En esta etapa veremos todo lo relacionado con "POO"
+"""
+
+
+
+class Persona:
+    __nombre=None
+    __email=None
+    def __init__(self):
+        print("persona")
+
+class Alumno (Persona):
+     __nombre=None
+     __matricula=None
+     __carrera=None
+     def __init__(self):
+         super().__init__()
+         print("Alumno")
+
+objeto_persona= Persona()
+objeto_alumno= Alumno()
+
+objeto_persona.nombre= "Dejah"
+print(objeto_persona.nombre)
+
+objeto_alumno.nombre= "John Carter"
+print(objeto_alumno.nombre)
+
+objeto_alumno.email= "john@utectulancingo.edu.mx"
+print(objeto_alumno.email)
+
+print(dir(objeto_persona))
+print(dir(objeto_alumno))
